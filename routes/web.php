508debb3828908fd388a4abe437b6a25004e56e3
irvn <129978135+irvento@users.noreply.github.com>
2025-02-14 23:20:54 +0800

    anti dos all routes

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,23 @@
 use App\Http\Controllers\logsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => 'throttle: 5, .1'], function () {
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::group(['middleware'=>'throttle: 5, 1'], function(){
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified',
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
+
+
     Route::get('/admin/logs', [logsController::class, 'index'])->middleware('auth');
 });
 
