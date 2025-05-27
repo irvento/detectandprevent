585584b995f1be089a42f727bf92ffaed561b303
ast4rt3 <lawrenze3206@gmail.com>
2025-05-27 15:12:26 +0800

    Added commit logs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -27,6 +27,33 @@
                 @endforeach
             </tbody>
         </table>
+        <br><br>
+{{-- Commit Logs Table --}}
+<h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight text-center bg-green-600 p-4 rounded-lg shadow-lg mb-4">
+    {{ __('Commit Logs') }}
+</h2>
+<table class="w-full border-collapse border border-gray-700 mb-8">
+    <thead class="bg-green-700 text-white">
+        <tr>
+            <th class="p-3 text-left border border-gray-600">Commit Message</th>
+            <th class="p-3 text-left border border-gray-600">Commit Hash</th>
+            <th class="p-3 text-left border border-gray-600">Author</th>
+            <th class="p-3 text-left border border-gray-600">Date</th>
+        </tr>
+    </thead>
+    <tbody class="text-gray-300">
+        @foreach($commitLogs as $commit)
+        <tr class="hover:bg-green-800 transition">
+            <td class="p-3 border border-gray-600">{{ $commit['commit']['message'] }}</td>
+            <td class="p-3 border border-gray-600">
+                <a href="{{ $commit['html_url'] }}" class="text-blue-400" target="_blank">{{ substr($commit['sha'], 0, 7) }}</a>
+            </td>
+            <td class="p-3 border border-gray-600">{{ $commit['commit']['author']['name'] }}</td>
+            <td class="p-3 border border-gray-600">{{ \Carbon\Carbon::parse($commit['commit']['author']['date'])->toDayDateTimeString() }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 <br><br><br><br>
         <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight text-center bg-red-600 p-4 rounded-lg shadow-lg mb-4">
             {{ __('Error Logs') }}
